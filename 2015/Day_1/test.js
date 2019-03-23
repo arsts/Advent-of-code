ba990f389ebc68c81ffa1053484d2ac97d7ce88f
arsts <arstsu@gmail.com>
2019-03-23 20:41:15 +0300

    fix

--- 2015/Day_1/test.js
+++
@@ -1,8 +0,0 @@
-
-const readable = getReadableStreamSomehow();
-readable.on('readable', () => {
-    let chunk;
-    while (null !== (chunk = readable.read())) {
-        console.log(`Received ${chunk.length} bytes of data.`);
-    }
-});
